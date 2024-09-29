5c3b34ad2d632c0b14ea0c1c6fad2f304542a623
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-09-30 00:39:58 +1300

    doc: add javadoc for constants class

--- src/main/java/scheduler/constants/Constants.java
+++ src/main/java/scheduler/constants/Constants.java
@@ -1,5 +1,8 @@
 package scheduler.constants;
 
+/**
+ * This class defines constants that are used throughout the project
+ */
 public class Constants {
     public static final int INF_32 = Integer.MAX_VALUE;
 }
