ac9ead89179d4e6d3e141db910f2ca1bdcc98737
Gallon Zhou <gallonzhou04@gmail.com>
2024-09-30 01:48:36 +1300

    Docs (Gallon): Added Java Docs For Main Driver Code

--- src/main/java/scheduler/Main.java
+++ src/main/java/scheduler/Main.java
@@ -9,7 +9,17 @@ import scheduler.parsers.CLIParser;
 import scheduler.parsers.InputOutputParser;
 import scheduler.schedulers.DFSScheduler;
 
+/**
+ * The Main Class contains the necessary driver code for ensuring our program runs smoothly, and that a valid and
+ * optimal schedule is generated. JavaFX code will also run if the user specifies that they want the schedule to be
+ * visualised.
+ */
 public class Main {
+    /**
+     * The main method for executing the main driver code.
+     *
+     * @param CLIArguments CLIArguments the arguments passed by the user
+     */
     public static void main(String[] CLIArguments) {
         Arguments arguments;
 
