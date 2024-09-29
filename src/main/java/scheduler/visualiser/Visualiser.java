b2b2c5656f10994ac7449dd970bf23b60bdb31ce
nlia656 <nlia656@aucklanduni.ac.nz>
2024-09-29 21:04:28 +1300

    added header to the screen

--- src/main/java/scheduler/visualiser/Visualiser.java
+++ src/main/java/scheduler/visualiser/Visualiser.java
@@ -19,30 +19,31 @@ public class Visualiser extends Application {
         Visualiser.arguments = arguments;
         launch();
     }
-    // private static Parent loadFxml(final String fxml) throws IOException {
-    //     FXMLLoader loader = new FXMLLoader(Visualiser.class.getResource("/fxml/" + fxml + ".fxml"));
-    //     Parent root = loader.load();
+    private static Parent loadFxml(final String fxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Visualiser.class.getResource("/fxml/" + fxml + ".fxml"));
+        Parent root = loader.load();
         
-    //     ProcessorController controller = loader.getController();
-    //     controller.setArguments(arguments);
-    //     return root;
-    // }
+        ProcessorController controller = loader.getController();
+        controller.setArguments(arguments);
+        return root;
+    }
     // Used for milestone 2
 
     //for milestone 2
     // private static Parent loadFxml(final String fxml) throws IOException {
-    // return new FXMLLoader(Visualiser.class.getResource("/fxml/" + fxml + ".fxml")).load();
+    //     return new FXMLLoader(Visualiser.class.getResource("/fxml/" + fxml + ".fxml")).load();
     // }
 
     @Override
     public void start(Stage stage) throws IOException {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        // String javaVersion = System.getProperty("java.version");
+        // String javafxVersion = System.getProperty("javafx.version");
+        // Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         //Scene scene = new Scene(loadFxml("visualiser"), 1280, 720);
         //above is for milestone 2
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        // Scene scene = new Scene(loadFxml("processor"), 1280, 720);
+        //Scene scene = new Scene(new StackPane(l), 640, 480);
+        
+        Scene scene = new Scene(loadFxml("processor"), 1280, 720);
         // Used for milestone 2
         stage.setScene(scene);
         stage.show();
