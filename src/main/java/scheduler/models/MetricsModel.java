8c32f165ff8418df75110a234461675243eaff3c
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-10-07 02:43:27 +1300

    feat: add memory to metrics model class

--- src/main/java/scheduler/models/MetricsModel.java
+++ src/main/java/scheduler/models/MetricsModel.java
@@ -8,6 +8,7 @@ public class MetricsModel {
     private int numberOfClosedStates;
 
     private double elapsedTime;
+    private double memoryUsed;
 
     public MetricsModel() {
         this.numberOfOpenedStates = 0;
@@ -50,11 +51,20 @@ public class MetricsModel {
         this.elapsedTime = elapsedTime;
     }
 
+    public double getMemoryUsed() {
+        return this.memoryUsed;
+    }
+
+    public void setMemoryUsed(double memoryUsed) {
+        this.memoryUsed = memoryUsed;
+    }
+
     public void display() {
         System.out.println("Metrics:");
         System.out.printf("  %-25s %ss%n", "Elapsed time in seconds:", elapsedTime);
         System.out.printf("  %-25s %d%n", "Number of opened states:", this.numberOfOpenedStates);
         System.out.printf("  %-25s %d%n", "Number of closed states:", this.numberOfClosedStates);
         System.out.printf("  %-25s %d%n", "Schedule finish time:", this.bestState.getMaximumFinishTime());
+        System.out.printf("  %-25s %.3fMB%n", "Memory used in MB:", this.memoryUsed / (1024 * 1024));
     }
 }
