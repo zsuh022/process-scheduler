2ed3cd867ff90806abee09cb877d2199ed185b03
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-10-10 20:58:30 +1300

    feat: add ids to line charts, add to controller with initialise method

--- src/main/java/visualiser/controllers/DynamicController.java
+++ src/main/java/visualiser/controllers/DynamicController.java
@@ -1,10 +1,30 @@
 package visualiser.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import scheduler.models.MetricsModel;
 
 public class DynamicController {
 
     @FXML
     private Label lblTimeElapsed;
+
+    @FXML
+    private LineChart<String, Number> lineChartRam;
+
+    @FXML
+    private LineChart<String, Number> lineChartCpu;
+
+    private XYChart.Series<String, Number> seriesRam = new XYChart.Series<>();
+    private XYChart.Series<String, Number> seriesCpu = new XYChart.Series<>();
+
+    private MetricsModel metricsModel;
+
+    @FXML
+    public void initialize() {
+        lineChartRam.getData().addAll(seriesRam);
+        lineChartCpu.getData().addAll(seriesCpu);
+    }
 }
