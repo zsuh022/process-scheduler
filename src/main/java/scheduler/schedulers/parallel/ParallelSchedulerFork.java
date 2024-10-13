199ef57165d98f007575adb72988f7e8544fb6b4
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-10-13 23:05:10 +1300

    feat: create initial class for fork join framework

---
+++ src/main/java/scheduler/schedulers/parallel/ParallelSchedulerFork.java
@@ -0,0 +1,33 @@
+package scheduler.schedulers.parallel;
+
+import scheduler.models.GraphModel;
+import scheduler.models.StateModel;
+import scheduler.schedulers.sequential.AStarScheduler;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+
+public class ParallelSchedulerFork extends AStarScheduler {
+    private final ForkJoinPool forkJoinPool;
+
+    private final PriorityQueue<StateModel> initialStates;
+
+    private final Set<StateModel> closedStates;
+
+    private final Object bestStateLock;
+
+    public ParallelSchedulerFork(GraphModel graph, byte processors, byte cores) {
+        super(graph, processors);
+
+        this.forkJoinPool = new ForkJoinPool(cores);
+
+        this.initialStates = new PriorityQueue<>(Comparator.comparingInt(this::getFCost));
+
+        this.closedStates = ConcurrentHashMap.newKeySet();
+
+        this.bestStateLock = new Object();
+    }
+}
