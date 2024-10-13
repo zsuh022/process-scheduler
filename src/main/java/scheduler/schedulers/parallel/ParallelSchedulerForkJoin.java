1a631deb8811460399897b04d670df049f8deff1
zsuh022 <zsuh022@aucklanduni.ac.nz>
2024-10-13 23:23:56 +1300

    feat: rename file to parallel scheduler fork join
    feat: add inner parallel scheduler class

--- src/main/java/scheduler/schedulers/parallel/ParallelSchedulerForkJoin.java
+++ src/main/java/scheduler/schedulers/parallel/ParallelSchedulerForkJoin.java
@@ -4,31 +4,31 @@ import scheduler.models.GraphModel;
 import scheduler.models.StateModel;
 import scheduler.schedulers.sequential.AStarScheduler;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ParallelSchedulerFork extends AStarScheduler {
+public class ParallelSchedulerForkJoin extends AStarScheduler {
     private final ForkJoinPool forkJoinPool;
-
-    private final PriorityQueue<StateModel> initialStates;
 
     private final Set<StateModel> closedStates;
 
     private final Object bestStateLock;
 
-    public ParallelSchedulerFork(GraphModel graph, byte processors, byte cores) {
+    private final AtomicBoolean isBestStateFound;
+
+    public ParallelSchedulerForkJoin(GraphModel graph, byte processors, byte cores) {
         super(graph, processors);
 
         this.forkJoinPool = new ForkJoinPool(cores);
 
-        this.initialStates = new PriorityQueue<>(Comparator.comparingInt(this::getFCost));
-
         this.closedStates = ConcurrentHashMap.newKeySet();
 
         this.bestStateLock = new Object();
+
+        this.isBestStateFound = new AtomicBoolean(false);
     }
 
     @Override
@@ -42,9 +42,24 @@ public class ParallelSchedulerFork extends AStarScheduler {
 
     @Override
     public void schedule() {
-        // fork join
+        StateModel initialState = new StateModel(this.processors, this.numberOfNodes);
+
+        this.forkJoinPool.invoke(new ParallelScheduler(initialState));
 
         metrics.setBestState(this.bestState);
         metrics.setNumberOfClosedStates(this.closedStates.size());
+    }
+
+    private class ParallelScheduler extends RecursiveTask<Void> {
+        private final StateModel state;
+
+        public ParallelScheduler(StateModel state) {
+            this.state = state;
+        }
+
+        @Override
+        protected Void compute() {
+            return null;
+        }
     }
 }
